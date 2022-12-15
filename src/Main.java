08c83dbf9aa474c3a45dc15c4652e6f6908dfa27
tony-you-sout <tanvuu998@gmail.com>
2022-12-15 20:00:53 +0700

    add

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int [] arr={1,2,3,4,5};
 
+       int []newArr= insert(arr,99,1);
+        for(int ele:newArr){
+            System.out.println(ele);
+        }
+
+
+
+    }
+    public static int [] insert(int arr[],int value,int postion){
+        int arrIndex=arr.length-1;
+        int tempIndex=arr.length;
+        int tempArr[] =new int [tempIndex+1];
+        boolean isInserted=false;
+        if(postion==0){
+            tempArr[0]=value;
+            for(int i=1;i<tempArr.length;i++){
+                tempArr[i]=arr[i-1];
+            }
+        }
+        else {
+            tempArr[postion]=value;
+            for(int j=0;j<postion;j++){
+                tempArr[j]=arr[j];
+            }
+            for(int k=postion+1;k<tempArr.length;k++){
+                tempArr[k]=arr[k-1];
+            }
+            tempArr[postion]=value;
+        }
+    return tempArr;
 
     }
 
---
+++ src/SuaBT.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class SuaBT {
+    public static void main(String [] args){
+        Scanner scan =new Scanner(System.in);
+//        Viết chương trình Java mà khi chạy, màn hình console sẽ cho phép ta nhập vào một số nguyên, in ra màn hình “Đây là số nguyên dương”
+//        Nếu số vừa nhập vào là một số lớn hơn hoặc bằng 0, ngược lại in ra “Đây là số nguyên âm”.
+//        System.out.println("nhập vào số nguyên n");
+//        int n=Integer.parseInt(scan.nextLine());
+//        bai1(n);
+
+//        Viết chương trình cho phép nhập vào một số nguyên dương n, tính tổng tất cả số chẵn trong khoảng từ 0 - n.
+//        int numberOfElement;
+//        System.out.println("nhập vào số phần tử mảng");
+//        numberOfElement=Integer.parseInt(scan.nextLine());
+//        int [] arr=new int[numberOfElement];
+//        for(int i=0;i<arr.length;i++){
+//            System.out.printf("nhập vào phần tử thứ %d\n",i);
+//            arr[i]=Integer.parseInt(scan.nextLine());
+//
+//        }
+//       int resultBai2= bai2(arr);
+//        System.out.printf("tong=%d",resultBai2);
+
+//        Viết chương trình để nhập nhập một số nguyên, tìm kết quả phép nhân của số đó với các số từ 1 - 20 , sau đó in kết quả ra màn hình.
+//        System.out.println("nhập vào số nguyên n");
+//        int number=Integer.parseInt(scan.nextLine());
+//        int[] result=bai3(number);
+//        for(int e:result){
+//            System.out.println(e);
+//        }
+//        Viết chương trình cho phép nhập vào một số nguyên n ( n < 1000 )
+//        In ra tất cả số nguyên trong khoảng từ 0 - n.
+        System.out.println("nhập số n <1000");
+        int n=Integer.parseInt(scan.nextLine());
+        while (n>=1000){
+            System.out.println("nhập số n <1000");
+            n=Integer.parseInt(scan.nextLine());
+
+        }
+        bai4(n);
+
+
+    }
+
+    public static void bai1(int n){
+        if(n>=0)
+            System.out.println("số dương");
+        else
+            System.out.println("số âm");
+
+    }
+    public static int bai2(int [] numbers){
+        int sum=0;
+        for(int i=0;i<numbers.length;i++){
+            if(numbers[i]%2==0)
+                sum+=numbers[i];//sum=sum+number[i]
+        }
+        return sum;
+
+    }
+    public static int [] bai3(int number){
+        int [] arrResult=new int[20];
+        int temp=0;
+        for(int i=0;i<20;i++){
+            temp=number*(i+1);
+            arrResult[i]=temp;
+        }
+        return arrResult;
+
+    }
+    public static void bai4(int number){
+        for(int i=0;i<=number;i++){
+            System.out.println(i);
+        }
+
+    }
+}
